56e3f6de75eed2753a2530f09150c26a2fc9ef89
Krooku <krokumeister@gmail.com>
2022-03-26 13:51:07 +0100

    deploy

--- src/app.js
+++ src/app.js
@@ -39,14 +39,14 @@ app.use(express.json({ limit: '50mb' }))
 app.use('/api', router)
 app.use('/uploads', express.static('uploads'))
 app.use('/', express.static(path.join(__dirname, '/../public/')))
-app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/../public/index.html')))
+// app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, '/../public/index.html')))
 
 // app.use(express.json())
 // app.disable('x-powered-by')
 
 // app.use('/api', router)
 // app.use('/static', express.static(path.join(__dirname, '/../public/static')))
-// app.get('/', function (req, res) {
-//   res.sendFile('index.html', { root: path.join(__dirname, '/../public') })
-// })
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '/../public') })
+})
 module.exports = app
